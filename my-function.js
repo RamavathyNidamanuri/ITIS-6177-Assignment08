876448d9c80c16617bef4b7bcad4d124c270fa75
RamavathyNidamanuri <ramacse9@gmail.com>
2022-02-27 15:43:58 -0500

    Update my-function.js

--- my-function.js
+++ my-function.js
@@ -1,20 +1,20 @@
 exports.handler = async (event) => {
     console.log("request: " + JSON.stringify(event));
     // TODO implement
-    let resp_data;
+    let response_data;
     let status;
     if (event.queryStringParameters && event.queryStringParameters.keyword) {
         console.log("Received keyword: " + event.queryStringParameters.keyword);
         let keyword = event.queryStringParameters.keyword;
-        resp_data = "Ramavathy says " + keyword;
+        response_data = "Ramavathy says " + keyword;
         status= 200;
     } else {
-        resp_data = "set input parameter keyword";
+        response_data = "Please set input parameter keyword";
         status= 400;
         }
     const response = {
         statusCode: status,
-        body: JSON.stringify(resp_data)
+        body: JSON.stringify(response_data)
     };
     console.log("response: " + JSON.stringify(response))
     return response;
